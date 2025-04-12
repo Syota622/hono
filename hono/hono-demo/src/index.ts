75b6d1cac9ce2a1ce33c0627e4a7ad83dd2cbac4
Syota0622 <wvp360gqanukcqhg@gmail.com>
2025-04-12 15:54:57 +0900

    Add GET

--- hono/hono-demo/src/index.ts
+++ hono/hono-demo/src/index.ts
@@ -1,9 +1,46 @@
 import { Hono } from 'hono'
 
-const app = new Hono()
+import { prettyJSON } from "hono/pretty-json";
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+let blogPosts = [
+  {
+    id: "1",
+    title: "blog1",
+    content: "content1"
+  },
+  {
+    id: "2",
+    title: "blog2",
+    content: "content2"
+  },
+  {
+    id: "3",
+    title: "blog3",
+    content: "content3"
+  }
+]
+
+const app = new Hono();
+app.use("*", prettyJSON());
+
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
+
+// blogPostsからtitle、contentを取得して返す
+app.get("/entry/:id", (c) => {
+  const id = c.req.param("id");
+  const post = blogPosts.find((post) => post.id === id);
+  if (!post) {
+    return c.json({
+      "error": "post not found"
+    }, 404);
+  }
+  return c.json({
+    "you id is": id,
+    "title": post.title,
+    "content": post.content
+  });
+});
 
 export default app
